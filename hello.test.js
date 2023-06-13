20c30c65f4088998afa504403eec1406a7156341
etapiab <etapiab@gmail.com>
2023-06-13 18:47:21 -0500

    cambio de idioma

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola Mundo, Saludos desde Peru");
+    expect(hello.hello()).toEqual("Hola Mundo, saludos desde Peru");
   });
 });
